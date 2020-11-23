61c4169668d57223d21788f226b024ac8ab4e879
Zoeinfp <zoebelleton@gmail.com>
2020-11-23 17:55:16 +0100

    ProductController 3

--- src/main/java/com/zoe/commerce/controllers/ProductController.java
+++ src/main/java/com/zoe/commerce/controllers/ProductController.java
@@ -3,14 +3,11 @@ package com.zoe.commerce.controllers;
 import com.zoe.commerce.repositories.ProductRepository;
 import com.zoe.commerce.models.Product;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@Controller
-@RequestMapping("/produit")
+@RestController
 public class ProductController {
 
     private final ProductRepository productRepository;
@@ -19,12 +16,12 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    @GetMapping(value="/{id}")
     public Optional<Product> findProductById(@PathVariable("id") Long id) {
         return productRepository.findById(id);
     }
 
-    @RequestMapping(value="/{name}", method= RequestMethod.GET)
+    @GetMapping(value="/{name}")
     public Optional<Product> findProductByName(@PathVariable("name") String name){
         return productRepository.findByName(name);
     }
