e213760c19d86755df84c928b840c784437016f4
Zoeinfp <zoebelleton@gmail.com>
2020-11-23 09:55:53 +0100

    version

--- src/main/java/com/zoe/commandes/CommandsApplication.java
+++ src/main/java/com/zoe/commandes/CommandsApplication.java
@@ -1,4 +1,4 @@
-package com.example.commandes;
+package com.zoe.commandes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/zoe/commandes/CommandsApplicationTests.java
+++ src/test/java/com/zoe/commandes/CommandsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.commandes;
+package com.zoe.commandes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
