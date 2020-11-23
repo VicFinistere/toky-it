ee3983e10edb10ddd43b4ac15d028e1a27d0b769
Zoeinfp <zoebelleton@gmail.com>
2020-11-23 09:47:07 +0100

    first commit

---
+++ src/main/java/com/example/commandes/CommandsApplication.java
@@ -0,0 +1,21 @@
+package com.example.commandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class CommandsApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandsApplication.class, args);
+	}
+}
