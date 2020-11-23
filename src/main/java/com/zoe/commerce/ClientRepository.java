af33fae9f165b4945df16926b4de833b2fc12d59
Zoeinfp <zoebelleton@gmail.com>
2020-11-23 17:07:36 +0100

    new package for models

--- src/main/java/com/zoe/commerce/ClientRepository.java
+++ src/main/java/com/zoe/commerce/ClientRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ClientRepository extends CrudRepository<ClientRepository, Long> {
-    List<Product> findByName(String name);
+public interface ClientRepository extends CrudRepository<Client, Long> {
+    List<Client> findByName(String name);
     Client findById(long id);
 }
