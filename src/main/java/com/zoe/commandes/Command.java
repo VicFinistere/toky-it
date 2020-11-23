971ba1deea8ccd9e5930a16492b9373be107a3c5
Zoeinfp <zoebelleton@gmail.com>
2020-11-23 11:01:36 +0100

    first repository

---
+++ src/main/java/com/zoe/commandes/Command.java
@@ -0,0 +1,48 @@
+package com.zoe.commandes;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Command {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date date;
+    private boolean checked;
+
+    protected Command(){}
+
+    public Command(Date date){
+        this.date = date;
+        this.checked = false;
+    }
+
+    public Command(Date date, Boolean checked){
+        this.date = date;
+        this.checked = checked;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Command[id=%d, date='%s', checked='%s']",
+                id, date, checked);
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public boolean isChecked(){
+        return checked;
+    }
+}
--- src/main/java/com/zoe/commandes/CommandsApplication.java
+++ src/main/java/com/zoe/commandes/CommandsApplication.java
@@ -1,13 +1,21 @@
 package com.zoe.commandes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @Controller
 @SpringBootApplication
 public class CommandsApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(CommandsApplication.class);
 
 	@RequestMapping("/")
 	@ResponseBody
@@ -17,5 +25,46 @@ public class CommandsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommandsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CommandRepository repository) {
+		return (args) -> {
+
+			Date today = new Date();
+
+			// save a few Commands
+			repository.save(new Command(today));
+			repository.save(new Command(today));
+			repository.save(new Command(today, true));
+			repository.save(new Command(today, false));
+			repository.save(new Command(today));
+
+			// fetch all Commands
+			log.info("Commands found with findAll():");
+			log.info("-------------------------------");
+			for (Command Command : repository.findAll()) {
+				log.info(Command.toString());
+			}
+			log.info("");
+
+			// fetch an individual Command by ID
+			Command Command = repository.findById(1L);
+			log.info("Command found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(Command.toString());
+			log.info("");
+
+			// fetch Commands by last name
+			log.info("Command found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByDate(today).forEach(bauer -> {
+				log.info(bauer.toString());
+			});
+			// for (Command bauer : repository.findByLastName("Bauer")) {
+			//  log.info(bauer.toString());
+			// }
+			log.info("");
+		};
 	}
 }
