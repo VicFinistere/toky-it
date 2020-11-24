7ee0c7fce034710cfbe64bd1b93ad2273ed4dcbd
Zoeinfp <zoebelleton@gmail.com>
2020-11-24 10:40:57 +0100

    Gestion du chemin

--- src/main/java/com/zoe/commerce/controller/ProductController.java
+++ src/main/java/com/zoe/commerce/controller/ProductController.java
@@ -5,29 +5,26 @@ import com.zoe.commerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/produit")
 public class ProductController {
 
     @Autowired
     private ProductRepository repository;
 
-    @GetMapping(value = "/product")
-    public String product() {
-        return "C'est la page des produits";
-    }
 
-    @GetMapping(value = "/product/test/{id}")
-    public String findProductById(@PathVariable("id") int id) {
-        return "product : " + id;
-    }
-
-    @GetMapping(value = "/produit/{id}")
+    @GetMapping(value = "/{id}")
     public Optional<Product> findProductById(@PathVariable("id") Long id) {
         return repository.findById(id);
     }
 
+    @GetMapping(value = "/{name}")
+    public Optional<Product> findProductByName(@PathVariable("name") String name) {
+        return repository.findByName(name);
+    }
 }
