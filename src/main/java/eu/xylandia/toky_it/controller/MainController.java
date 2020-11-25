9063c92cf19ee35df9b3950559837efd522f31ee
Zoeinfp <zoebelleton@gmail.com>
2020-11-25 10:50:29 +0100

    controller

--- src/main/java/eu/xylandia/toky_it/controller/MainController.java
+++ src/main/java/eu/xylandia/toky_it/controller/MainController.java
@@ -2,26 +2,27 @@ package eu.xylandia.toky_it.controller;
 
 import eu.xylandia.toky_it.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @Autowired
     private PersonRepository personRepository;
 
-    @RequestMapping(value = "/")
-    public String index() {
-        return "Hello World";
-    }
-
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        return "index";
+    }
+
+    @RequestMapping("/index")
+    public String getIndex() {
         return "index";
     }
 }
