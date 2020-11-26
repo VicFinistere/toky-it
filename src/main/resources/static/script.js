b609d3f20ed05fd35f69ebf4acef58a04acaffe8
Zoeinfp <zoebelleton@gmail.com>
2020-11-26 13:24:58 +0100

    script.js

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,179 @@
+/* Event listener : Getting answer using button */
+$(function() { $('#tchat_button').on('click', function() { sending_text(); }); });
+
+/* Event listener : Getting answer pressing enter */
+$(function () { $(document).keypress(function (e) { if (e.which === 13){ sending_text(); }}); });
+
+/* Robot Introduction */
+$(function(){ introduction();});
+
+function scroll()
+{
+    //Debug
+    console.debug("Scrolling down the conversation (messaging.js)");
+
+    var tchat_area = document.getElementById('tchat_area');
+    tchat_area.scrollTop = tchat_area.scrollHeight;
+}
+
+function introduction()
+{
+    //Debug
+    console.debug("Message introduction (messaging.js)");
+    var d = new Date();
+    var timer = d.toLocaleTimeString();
+    $('<p>', {class: 'robot_white_msg', text: "Hello !"}).appendTo('#tchat_area');
+    $("<p class='text-center'><small style='font-size:10px;margin-right: 65%;'>"+timer+"</small></p>").appendTo('#tchat_area');
+    $('<p>', {class: 'robot_white_msg', text: "Let's tchat !"}).appendTo('#tchat_area');
+    $("<p class='text-center'><small style='font-size:10px;margin-right: 65%;'>"+timer+"</small></p>").appendTo('#tchat_area');
+
+    scroll();
+}
+
+function clear_text()
+{
+    //Debug
+    console.debug("clear_text (messaging.js)");
+
+    if ($("#text_error").not(':empty'))
+    {
+        //Debug
+        console.debug("Text_error is cleared (messaging.js)");
+        $("#text_error").empty();
+    }
+
+    if ($("#text_place_area").not(':empty'))
+    {
+        //Debug
+        console.debug("Text_place is cleared (messaging.js)");
+        $("#text_place_area").empty();
+    }
+}
+
+function sending_text()
+{
+    //Debug
+    console.debug("sending_text (messaging.js)");
+
+    clear_text();
+
+    //Getting the input value
+    var input_text = $("#input_text").val();
+
+    //Getting the content in page
+    var content = $("#answer").html();
+
+    //If the input is filled
+    if(input_text !== "")
+    {
+        //We append the input text
+        $('<p>', { class: 'user_blue_msg', text: input_text}).appendTo('#tchat_area');
+        var d = new Date();
+        var timer = d.toLocaleTimeString();
+        $("<small style='font-size:10px;margin:0 auto; margin-left: 65%;'>"+timer+"</small>").appendTo('#tchat_area');
+        scroll();
+        answer();
+    }
+
+    //If the input if empty
+    else
+    {
+        //Warn
+        console.debug("No message in input text ! (messaging.js)");
+
+        //It has to be something in input
+        var text = "Ask something!...";
+
+        //If it's not already said
+        if(content !== text)
+        {
+            //Debug
+            console.debug("Giving empty input text error(messaging.js)");
+
+            //Say it!
+            $("#text_error").html(text);
+        }
+    }
+}
+
+function answer()
+{
+    //Debug
+    console.debug("answer (messaging.js))");
+
+    var user_input = $('#tchat_area').children('p.user_blue_msg').last();
+    var user_text = user_input.html();
+    var user_checked_text = user_text.toLowerCase();
+
+    bot_answering(user_checked_text);
+
+    //Input text value to null
+    $("#input_text").val("");
+
+}
+
+var user_input = $('#tchat_area').children('p.user_blue_msg').last();
+var robot_input = $('#tchat_area').children('p.robot_white_msg').last();
+
+var sentences = ['bonjour', 'salut', 'coucou'];
+var answers = [['coucou', 'bonjour', 'salut'], [], []];
+
+function bot_answering(user_checked_text)
+{
+    //Log
+    console.log("bot_answering (bot_answer.js)");
+    $.getJSON($SCRIPT_ROOT + '/get_dialog', {input:user_checked_text},
+        function (response)
+        {
+            //Debug
+            console.debug("Using mail personnal API");
+            if (response)
+            {
+                bot_response(response);
+                scroll();
+
+            }
+            else
+            {
+                bot_response(user_checked_text);
+                scroll();
+            }
+        });
+    // function get_random_answer(user_checked_text) {
+    //     for(var i = 0; i < sentences.length; i++){
+    //         if(sentences[i] === user_checked_text ){
+    //             var options = answers[i];
+    //             var response = options[Math.floor(Math.random() * options.length)];
+    //         }
+    //     }
+    //     if(response){
+    //         return response
+    //     } else {
+    //         return user_checked_text;
+    //     }
+    // }
+
+    if (true === user_checked_text.indexOf(sentences) !== -1) {
+        var random_answer = get_random_answer(user_checked_text);
+        bot_response(random_answer);
+    } else {
+        bot_response('...');
+    }
+    scroll();
+}
+
+function bot_response(response){
+    if(user_input.css('color') !== 'rgb(230, 230, 230)'){
+        $('<p>', {class: 'robot_white_msg', text:response}).appendTo('#tchat_area');
+        var d = new Date();
+        var timer = d.toLocaleTimeString();
+        $("<p class='text-center'><small style='font-size:10px;margin-right: 65%;'>"+timer+"</small></p>").appendTo('#tchat_area');
+        user_input.css('color', 'rgb(230, 230, 230)');
+    }
+}
+
+function askRandomly() {
+    $.get( "https://opentdb.com/api.php?amount=1&category=18&type=multiple", function( data ) {
+        $("#chat-input").val(data.results[0].question);
+    });
+}
