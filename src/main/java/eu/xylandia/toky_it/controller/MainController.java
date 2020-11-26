fcd097cbc294e4477b989823a37e7849a9354ac8
Zoeinfp <zoebelleton@gmail.com>
2020-11-26 14:57:29 +0100

    answer

--- src/main/java/eu/xylandia/toky_it/controller/MainController.java
+++ src/main/java/eu/xylandia/toky_it/controller/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
@@ -15,14 +14,11 @@ public class MainController {
     @Autowired
     private PersonRepository personRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "index";
-    }
-
     @RequestMapping("/index")
     public String getIndex() {
-        return "index";
+        return "ask";
     }
+
+    @GetMapping("/answer")
+    public String getAnswer() { return "answer"; }
 }
