5a15eab9a340c491488e91304691867c3e9a682a
Zoeinfp <zoebelleton@gmail.com>
2020-11-27 16:15:20 +0100

    html for div content

--- src/main/resources/static/answer.js
+++ src/main/resources/static/answer.js
@@ -1,15 +1,13 @@
+
+
 function get_questions() {
     $.get("/getQuestions", function (questions) {
         console.log(questions);
 
         $.each(questions, function (i, question) {
             let questions_area = $("#chat-area");
-            if (questions_area.val()) {
-                let already_asked_questions = questions_area.val();
-                if (!already_asked_questions.includes(question)) {
-                    $('<p>', {class: 'robot_info_msg', text: question}).appendTo('#chat_area');
-                }
-            } else {
+            let already_asked_questions = questions_area.html();
+            if(!already_asked_questions.includes(question)) {
                 $('<p>', {class: 'robot_info_msg', text: question}).appendTo('#chat_area');
             }
         });
