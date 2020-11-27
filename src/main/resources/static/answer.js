af652d10cef3acc76e14acf400971d8268302b2a
Zoeinfp <zoebelleton@gmail.com>
2020-11-27 15:41:12 +0100

    set interval

--- src/main/resources/static/answer.js
+++ src/main/resources/static/answer.js
@@ -1,13 +1,17 @@
 
 
-$(function () {
+function get_questions() {
     $.get("/getQuestions", function (questions) {
         console.log(questions);
 
         $.each(questions, function (i, question) {
             let questions_area = $("#questions-area");
             let questions = questions_area.val();
-            questions_area.val(questions + "\n" + question);
+            if(!questions.includes(question)) {
+                questions_area.val(questions + "\n" + question);
+            }
         });
     });
-});
+}
+
+setInterval(get_questions, 10000);
