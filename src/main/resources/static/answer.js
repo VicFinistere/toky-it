0807a21e6f21b84b1da64aec06499a846e6a6f6b
Zoeinfp <zoebelleton@gmail.com>
2020-11-27 16:03:11 +0100

    new question in bubble 2

--- src/main/resources/static/answer.js
+++ src/main/resources/static/answer.js
@@ -5,7 +5,7 @@ function get_questions() {
         console.log(questions);
 
         $.each(questions, function (i, question) {
-            let questions_area = $("#questions-area");
+            let questions_area = $("#chat-area");
             let already_asked_questions = questions_area.val();
             if(!already_asked_questions.includes(question)) {
                 $('<p>', {class: 'robot_info_msg', text: question}).appendTo('#chat_area');
