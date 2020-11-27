f3fedd37dbf0fbb530f63dcd3bd6838390db4589
Zoeinfp <zoebelleton@gmail.com>
2020-11-27 16:12:57 +0100

    new question in bubble 4

--- src/main/resources/static/answer.js
+++ src/main/resources/static/answer.js
@@ -1,13 +1,15 @@
-
-
 function get_questions() {
     $.get("/getQuestions", function (questions) {
         console.log(questions);
 
         $.each(questions, function (i, question) {
             let questions_area = $("#chat-area");
-            let already_asked_questions = questions_area.val();
-            if(!already_asked_questions.includes(question)) {
+            if (questions_area.val()) {
+                let already_asked_questions = questions_area.val();
+                if (!already_asked_questions.includes(question)) {
+                    $('<p>', {class: 'robot_info_msg', text: question}).appendTo('#chat_area');
+                }
+            } else {
                 $('<p>', {class: 'robot_info_msg', text: question}).appendTo('#chat_area');
             }
         });
