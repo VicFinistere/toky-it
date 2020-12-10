e40c4a1adc8a02c75367afc0112b8278fb7cb6d0
Zoe <zoebelleton@gmail.com>
2020-12-10 15:45:07 +0100

    https

--- src/main/resources/static/stack-exchange.js
+++ src/main/resources/static/stack-exchange.js
@@ -11,7 +11,7 @@ $.get("/getStackKey", function (stack_key) {
         clientId: 1,
         key: stack_key,
         // Used for cross domain communication, it will be validated
-        channelUrl: 'http://toky-ok-it.herokuapp.com/blank',
+        channelUrl: 'https://toky-ok-it.herokuapp.com/blank',
         // Called when all initialization is finished
         complete: function (data) {
             $('#login-button')
