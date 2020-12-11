462b2f11bed6f1575b6c007e95b0e0a8c6da87e5
Zoe <zoebelleton@gmail.com>
2020-12-11 15:57:44 +0100

    username

--- src/main/java/eu/xylandia/toky_it/SecurityConfig.java
+++ src/main/java/eu/xylandia/toky_it/SecurityConfig.java
@@ -5,7 +5,11 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.sql.DataSource;
 
@@ -20,6 +24,13 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/")
                 .permitAll();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            UserDetails userPrincipal = (UserDetails)authentication.getPrincipal();
+            System.out.println("User principal name =" + userPrincipal.getUsername());
+            System.out.println("Is user enabled =" + userPrincipal.isEnabled());
+        }
     }
 }
 
