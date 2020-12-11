eb053a14074b20d0bdac240e4b57fe54bd37d562
Zoe <zoebelleton@gmail.com>
2020-12-11 15:39:16 +0100

    login page

---
+++ src/main/java/eu/xylandia/toky_it/SecurityConfig.java
@@ -0,0 +1,25 @@
+package eu.xylandia.toky_it;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/").hasRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .permitAll();
+    }
+}
+
