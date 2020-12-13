1f8337f821152175237ad1d79f94bef0cac6dca4
Zoe <zoebelleton@gmail.com>
2020-12-13 11:20:02 +0100

    principal tostring

--- src/main/java/eu/xylandia/toky_it/controller/MainController.java
+++ src/main/java/eu/xylandia/toky_it/controller/MainController.java
@@ -1,15 +1,12 @@
 package eu.xylandia.toky_it.controller;
 
 import eu.xylandia.toky_it.repositories.PersonRepository;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.Optional;
 
 @Controller
@@ -17,17 +14,13 @@ public class MainController {
 
     @RequestMapping("/")
     public String getIndex(Model model) {
-        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username = user.getUsername();
-        model.addAttribute("username", username);
+        model.addAttribute("username",  SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return "index";
     }
 
     @GetMapping("/answer")
     public String getAnswer(Model model) {
-        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username = user.getUsername();
-        model.addAttribute("username", username);
+        model.addAttribute("username",  SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return "answer";
     }
 
